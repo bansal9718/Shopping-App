378f94d9b9419519ad9fea80d56969498bccc329
Rohit <rohitbansal4685@gmail.com>
2024-09-22 16:17:02 +0530

    url change in frontned

--- frontend/vite-project/src/axioxConfig.js
+++ frontend/vite-project/src/axioxConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://shopping-app-2-p00m.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
