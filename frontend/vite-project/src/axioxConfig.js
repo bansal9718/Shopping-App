0ab68265c6610fbf2c5425b9f8909d31431fbc30
Rohit <rohitbansal4685@gmail.com>
2024-09-23 20:46:13 +0530

    Deployed Backend

--- frontend/vite-project/src/axioxConfig.js
+++ frontend/vite-project/src/axioxConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-595s.onrender.com//api",
+  baseURL: "https://backend-595s.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
